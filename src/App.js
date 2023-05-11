5311776846eaa1afb082752771491c0299e656fe
skabeo <spencerokyere95@gmail.com>
2023-05-11 14:44:21 +0100

    Modify main app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Categories from './components/Categories';
 
 function App() {
   return (
-    <div>
+    <div className="library">
       <Navbar />
       <Routes>
         <Route path="/" element={<Bookapp />} />
